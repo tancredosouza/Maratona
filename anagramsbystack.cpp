4182e7eca9c84436b0a052a2f8b1ac074b039e80
Tancredo <tjssj@cin.ufpe.br>
2017-02-19 17:47:16 -0300

    AC Anagrams by Stack

--- anagramsbystack.cpp
+++ anagramsbystack.cpp
@@ -6,28 +6,48 @@ int pos_in, pos_res;
 
 void func(stack<char> stk, int c, int pos, string str) {
 
-	if(c == in.size()) {
-		if (res.size() == pos)
-			cout << str << endl;
-
-		else 
-			return;
+	if(stk.empty() && pos == res.size()) {
+		if(!str.empty())
+			str.erase(str.end() - 1);
+		cout << str << endl;
 	}
+
+	else if((!stk.empty() && c > in.size()))
+		return;
 
 	else if (stk.empty() || stk.top() != res[pos]) {
-		stk.push(in[++c]);
+		stk.push(in[c]);
 		str.push_back('i');
 		str.push_back(' ');
-		func(stk, c, pos, str);
+
+		func(stk, c + 1, pos, str);
 	}
 
-	else {
-		str.push_back('o');
-		str.push_back(' ');
-		stk.pop();
-		func(stk, c + 1, pos + 1, str);
+	else if (!stk.empty() && stk.top() == res[pos]) {
+		string res;
+		if(c < in.size()) {// in
+			res = str;
+			stk.push(in[c]);
+			str.push_back('i');
+			str.push_back(' ');
+		
+			func(stk, c + 1, pos, str);
+			stk.pop();
+			str = res;
+		}
+
+		//out
+		if(!stk.empty()) {
+			stk.pop();
+			str.push_back('o');
+			str.push_back(' ');
+
+			func(stk, c, pos + 1, str);
+		}
 	}
 
+	else
+		return;
 }
 
 int main() {
@@ -37,7 +57,7 @@ int main() {
 		stack<char> anag;
 		printf("[\n");
 		func(anag, 0, 0, s);
-		printf("\n]\n");
+		printf("]\n");
 	}
 	return 0;
 }
